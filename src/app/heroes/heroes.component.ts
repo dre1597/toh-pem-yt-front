b52c7b6d5b2854d250cc2feccd50270377c7fdcd
André Pereira <andrepereira97@yahoo.com>
2023-10-13 20:07:47 -0300

    :sparkles: add heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero.type';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,8 +8,15 @@ import { Hero } from '../hero.type';
   styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent {
+  protected heroes = HEROES;
+  protected selectedHero?: Hero;
+
   protected hero: Hero = {
     id: 1,
     name: 'Windstorm',
   };
+
+  protected onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
