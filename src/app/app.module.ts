a21170f2773711429d2a3b46ec3a59a7f399a31a
André Pereira <andrepereira97@yahoo.com>
2023-10-16 14:00:37 -0300

    :sparkles: add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 const materialModules = [
   MatToolbarModule,
   MatButtonModule,
   MatExpansionModule,
   MatListModule,
+  MatIconModule,
+  MatTooltipModule,
 ];
 
 @NgModule({
@@ -32,6 +36,7 @@ const materialModules = [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     ...materialModules,
   ],
   providers: [],
