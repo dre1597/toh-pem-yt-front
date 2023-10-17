c571febd47ebe7e197ead82c596bea60d1f64a6e
André Pereira <andrepereira97@yahoo.com>
2023-10-17 00:07:43 -0300

    :sparkles: add logic to only import core module once

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
@@ -13,4 +13,12 @@ import { NotFoundPageComponent } from './components/not-found-page/not-found-pag
   imports: [CommonModule, MaterialModule, RouterLink],
   exports: [MessagesComponent, ToolbarComponent],
 })
-export class CoreModule {}
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule?: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only',
+      );
+    }
+  }
+}
