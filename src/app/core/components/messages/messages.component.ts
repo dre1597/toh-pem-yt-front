9b89992798fcb8b4f2803542a72722308f8eb168
André Pereira <andrepereira97@yahoo.com>
2023-10-16 23:50:52 -0300

    :fire: remove unused styles files

--- src/app/core/components/messages/messages.component.ts
+++ src/app/core/components/messages/messages.component.ts
@@ -5,7 +5,6 @@ import { MessagesService } from '../../services/messages.service';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent {
   protected readonly messagesService = inject(MessagesService);
