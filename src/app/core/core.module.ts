a5e0463d0991b79a400200c7338cfa80c7c3d814
André Pereira <andrepereira97@yahoo.com>
2023-10-18 21:14:30 -0300

    :sparkles: add loading

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,33 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { MaterialModule } from '../material/material.module';
 
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { LoadingComponent } from './components/loading/loading.component';
+
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
 
 @NgModule({
-  declarations: [MessagesComponent, ToolbarComponent, NotFoundPageComponent],
+  declarations: [
+    MessagesComponent,
+    ToolbarComponent,
+    NotFoundPageComponent,
+    LoadingComponent,
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoadingInterceptor,
+      multi: true,
+    },
+  ],
   imports: [CommonModule, MaterialModule, RouterLink],
-  exports: [MessagesComponent, ToolbarComponent],
+  exports: [MessagesComponent, ToolbarComponent, LoadingComponent],
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parentModule?: CoreModule) {
---
+++ src/app/core/interceptors/loading.interceptor.ts
@@ -0,0 +1,37 @@
+import { inject, Injectable } from '@angular/core';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { finalize, Observable } from 'rxjs';
+
+import { LoadingService } from '../services/loading.service';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+  private activeRequests = 0;
+  private loadingService = inject(LoadingService);
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<unknown>> {
+    if (this.activeRequests === 0) {
+      this.loadingService.show();
+    }
+
+    this.activeRequests++;
+
+    return next.handle(request).pipe(
+      finalize(() => {
+        this.activeRequests--;
+
+        if (this.activeRequests === 0) {
+          this.loadingService.hide();
+        }
+      }),
+    );
+  }
+}
